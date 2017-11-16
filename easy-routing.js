b597f7720e30a1ec3fcd10973492a96446b8f5c1
thegunner686 <gunn686@gmail.com>
2017-11-15 21:30:28 -0800

    exports fix again

--- easy-routing.js
+++ easy-routing.js
@@ -2,6 +2,8 @@ import Link from "./Link";
 import Route from "./Route";
 import IndexRoute from "./IndexRoute";
 
-exports.Link = Link;
-exports.Route = Route;
-exports.IndexRoute = IndexRoute;
+module.exports = {
+  Link,
+  Route,
+  IndexRoute
+};
