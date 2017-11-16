06aa6ed1b5b0374125d907472f68a3dc2f9e6b65
thegunner686 <gunn686@gmail.com>
2017-11-15 21:33:18 -0800

    exports

--- easy-routing.js
+++ easy-routing.js
@@ -2,8 +2,10 @@ import Link from "./Link";
 import Route from "./Route";
 import IndexRoute from "./IndexRoute";
 
-module.exports = {
+let easy_routing = {
   Link,
   Route,
   IndexRoute
 };
+
+export default easy_routing;
