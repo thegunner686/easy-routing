7ce1715bc1e793fc0d208f815c9a5ce15484dd46
thegunner686 <gunn686@gmail.com>
2017-11-16 10:50:19 -0800

    added to exports

--- easy-routing.js
+++ easy-routing.js
@@ -1,9 +1,13 @@
 let Link = require("./Link").default,
+    ClassicLink = require("./ClassicLink").default,
+    OpacityLink = require("./OpacityLink").default,
     Route = require("./Route").default,
     IndexRoute = require("./IndexRoute").default;
 
 module.exports = {
   Link,
+  ClassicLink,
+  OpacityLink,
   Route,
   IndexRoute,
 };
